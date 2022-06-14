a0fb9ee2770d1a7352c3d9db9dc0e5f814aa5fd2
mgeri93 <48027734+mgeri93@users.noreply.github.com>
2022-06-14 18:26:29 +0200

    put checker back to Service and implement in save method

--- src/main/java/com/example/cisionrestapi/domain/Payload.java
+++ src/main/java/com/example/cisionrestapi/domain/Payload.java
@@ -23,44 +23,5 @@ public class Payload {
     private String timestamp;
 
     private int longestPalindromeSize;
-
-    public Payload(Long id, String content, String timestamp) {
-        this.id = id;
-        this.content = content;
-        this.timestamp = timestamp;
-        this.longestPalindromeSize = longestPalSize(content);
-    }
-
     public Payload(){}
-
-    public int longestPalSize(String content){
-        int n = content.length();
-        if (n < 2)
-            return n;
-
-        int maxLength = 1,start=0;
-        int low, high;
-        for(int i = 0; i < n; i++){
-            low = i - 1;
-            high = i + 1;
-            while(high < n && content.charAt(high) == content.charAt(i))
-                high++;
-
-            while(low >= 0 && content.charAt(low) == content.charAt(i))
-                low--;
-
-            while(low >= 0 && high < n && content.charAt(low) == content.charAt(high)){
-                low--;
-                high++;
-            }
-
-            int length = high - low - 1;
-            if(maxLength < length){
-                maxLength = length;
-                start=low+1;
-            }
-        }
-        return maxLength;
-    }
-
 }
--- src/main/java/com/example/cisionrestapi/service/PayloadService.java
+++ src/main/java/com/example/cisionrestapi/service/PayloadService.java
@@ -20,11 +20,46 @@ public class PayloadService {
     }
 
     public Payload save(Payload payload){
+        payload.setLongestPalindromeSize(longestPalSize(payload.getContent()));
         return payloadRepository.save(payload);
     }
 
     public Iterable<Payload> save(List<Payload> payloadList){
+        for(Payload payload : payloadList){
+            payload.setLongestPalindromeSize(longestPalSize(payload.getContent()));
+        }
         return payloadRepository.saveAll(payloadList);
     }
+
+    public int longestPalSize(String content){
+        int n = content.length();
+        if (n < 2)
+            return n;
+
+        int maxLength = 1,start=0;
+        int low, high;
+        for(int i = 0; i < n; i++){
+            low = i - 1;
+            high = i + 1;
+            while(high < n && content.charAt(high) == content.charAt(i))
+                high++;
+
+            while(low >= 0 && content.charAt(low) == content.charAt(i))
+                low--;
+
+            while(low >= 0 && high < n && content.charAt(low) == content.charAt(high)){
+                low--;
+                high++;
+            }
+
+            int length = high - low - 1;
+            if(maxLength < length){
+                maxLength = length;
+                start=low+1;
+            }
+        }
+        return maxLength;
+    }
+
 
 }
