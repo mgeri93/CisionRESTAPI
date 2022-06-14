c28ed35ac3767dad6bd5a37762086e9382921858
mgeri93 <48027734+mgeri93@users.noreply.github.com>
2022-06-14 17:42:27 +0200

    Update Payload.java
    Timestamp is now a String!

--- src/main/java/com/example/cisionrestapi/domain/Payload.java
+++ src/main/java/com/example/cisionrestapi/domain/Payload.java
@@ -1,28 +1,28 @@
 package com.example.cisionrestapi.domain;
 
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Timestamp;
 
+@Data
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
+
 public class Payload {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String content;
-    private Timestamp timestamp;
+    private String timestamp;
 
     public Payload(){}
-
 
 }
