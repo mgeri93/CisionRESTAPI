28fb3450cbe68061358ed807ac233cae7b573f14
mgeri93 <48027734+mgeri93@users.noreply.github.com>
2022-06-14 16:34:40 +0200

    Initial commit

---
+++ src/main/java/com/example/cisionrestapi/CisionRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cisionrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CisionRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CisionRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cisionrestapi/CisionRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cisionrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CisionRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
