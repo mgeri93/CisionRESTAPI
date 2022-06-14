54bf4f495d4155b2f018d3516c913362c315ac89
mgeri93 <48027734+mgeri93@users.noreply.github.com>
2022-06-14 17:20:47 +0200

    read&save payload file

--- src/main/java/com/example/cisionrestapi/CisionRestApiApplication.java
+++ src/main/java/com/example/cisionrestapi/CisionRestApiApplication.java
@@ -1,7 +1,17 @@
 package com.example.cisionrestapi;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.example.cisionrestapi.domain.Payload;
+import com.example.cisionrestapi.service.PayloadService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @SpringBootApplication
 public class CisionRestApiApplication {
@@ -10,5 +20,20 @@ public class CisionRestApiApplication {
         SpringApplication.run(CisionRestApiApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner runner(PayloadService payloadService){
+        return args -> {
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference<List<Payload>> typeReference = new TypeReference<List<Payload>>(){};
+            InputStream inputStream = TypeReference.class.getResourceAsStream("/json/payload.json");
+            try{
+                List<Payload> payloadList = mapper.readValue(inputStream,typeReference);
+                payloadService.save(payloadList);
+                System.out.println("Data Saved to database!");
+            } catch (IOException e){
+                System.out.println("Unable to save data: " + e.getMessage());
+            }
+        };
+    }
 
 }
