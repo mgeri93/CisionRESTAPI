92c32acb2d613e28a29e19ddbdd5e7c2070559cd
mgeri93 <48027734+mgeri93@users.noreply.github.com>
2022-06-14 18:08:04 +0200

    Creating the palindrome size checker

--- src/main/java/com/example/cisionrestapi/service/PayloadService.java
+++ src/main/java/com/example/cisionrestapi/service/PayloadService.java
@@ -26,4 +26,35 @@ public class PayloadService {
     public Iterable<Payload> save(List<Payload> payloadList){
         return payloadRepository.saveAll(payloadList);
     }
+
+    public int longestPalSize(String content){
+        int n = content.length();
+        if (n < 2)
+            return n;
+
+        int maxLength = 1,start=0;
+        int low, high;
+        for(int i = 0; i < n; i++){
+            low = i - 1;
+            high = i + 1;
+            while(high < n && content.charAt(high) == content.charAt(i))
+                high++;
+
+            while(low >= 0 && content.charAt(low) == content.charAt(i))
+                low--;
+
+            while(low >= 0 && high < n && content.charAt(low) == content.charAt(high)){
+                low--;
+                high++;
+            }
+
+            int length = high - low - 1;
+            if(maxLength < length){
+                maxLength = length;
+                start=low+1;
+            }
+        }
+        return maxLength;
+    }
+
 }
